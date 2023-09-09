a823aab1c687de3025f56b8a31e1c56705c8e282
RikihisaRyota <115054910+RikihisaRyota@users.noreply.github.com>
2023-09-09 20:54:02 +0900

    階段と平行を追加

--- Game/Player.h
+++ Game/Player.h
@@ -19,11 +19,24 @@ public:
 private:
     void Bounce();
 
+    void ComboUpdate(float floor, uint32_t x, uint32_t y);
+    void ParticleUpdate(uint32_t x, uint32_t y);
     // 真ん中
     Vector2 position_;
     Vector2 size_;
 
     Vector2 velocity_;
+    // コンボ
+    const uint32_t kCombo_ = 3;
+    // 階段
+    uint32_t preStep_;
+    uint32_t step_;
+    uint32_t stepCount_;
+    // 平行
+    uint32_t preSameHeight_;
+    uint32_t sameHeight_;
+    uint32_t sameHeightCount_;
+
 
     Field* field_;
     ParticleManager* particleManager_;
