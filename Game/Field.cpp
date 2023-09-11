99106a947278b2c86135fdf124b4d36641326d53
RikihisaRyota <115054910+RikihisaRyota@users.noreply.github.com>
2023-09-11 22:14:22 +0900

    デットライン追加

--- Game/Field.cpp
+++ Game/Field.cpp
@@ -43,6 +43,13 @@ void Field::Initialize() {
 	textureHandles_.at(Texture::kGrow) = TOMATOsEngine::LoadTexture("Resources/grow.png");
 	textureHandles_.at(Texture::kGameOverBlock) = TOMATOsEngine::LoadTexture("Resources/gameOverBlock.png");
 	textureHandles_.at(Texture::kGameOver) = TOMATOsEngine::LoadTexture("Resources/gameOver.png");
+	textureHandles_.at(Texture::kDeadLine) = TOMATOsEngine::LoadTexture("Resources/deadLine_1.png");
+
+	deadLinePosition_ = { float(TOMATOsEngine::kMonitorWidth) * 0.5f ,float(Field::kBlockSize) * float(kDeathLine_)/* + float(Field::kBlockSize) * 0.5f*/ };
+	deadLineSize_ = { 608.0f ,16.0f };
+	deadLineAnimationFront_ = 0.0f;
+	deadLineAnimationBack_ = deadLineSize_.x;
+	deadLineColorH_ = 0.025f;
 
 	heightCount_ = kDeathLine_;
 	blockBleakAnimationCount_ = 0;
@@ -50,7 +57,7 @@ void Field::Initialize() {
 
 	gameOverPosition_ = { float(TOMATOsEngine::kMonitorWidth) * 0.5f,float(TOMATOsEngine::kMonitorHeight) + float(TOMATOsEngine::kMonitorHeight) * 0.5f };
 	gameOverPositionStart_ = gameOverPosition_;
-	gameOverPositionEnd_ = { gameOverPosition_ .x,float(TOMATOsEngine::kMonitorHeight) * 0.5f };
+	gameOverPositionEnd_ = { gameOverPosition_.x,float(TOMATOsEngine::kMonitorHeight) * 0.5f };
 	dropTextCount_ = 0;
 	gameOverBlockCount_ = 0;
 
@@ -59,10 +66,10 @@ void Field::Initialize() {
 	isGameOver_ = false;
 	isInGameOver_ = false;
 	isVanish_ = false;
- 
-  
-    breakSoundHandle_ = TOMATOsEngine::LoadAudio("Resources/Audio/break1.wav");
-    lineBreakSoundHandle_ = TOMATOsEngine::LoadAudio("Resources/Audio/lineBreak.wav");
+
+
+	breakSoundHandle_ = TOMATOsEngine::LoadAudio("Resources/Audio/break1.wav");
+	lineBreakSoundHandle_ = TOMATOsEngine::LoadAudio("Resources/Audio/lineBreak.wav");
 }
 
 void Field::Update() {
@@ -89,45 +96,55 @@ void Field::Update() {
 		}
 	}
 	else
-	if (!isInGameOver_) {
-		ChackBlock();
-		++growCoolTime_;
-		breakTime_--;
-		if (growCoolTime_ >= growInterval_ && isFlash_ == false) {
-			// 成長
-			SetGrow(nextBlockIndices_, numGrowingBlocks_);
-			// 次成長するところをセット
-			nextBlockIndices_ = GetGrowField(numGrowingBlocks_);
-			growCoolTime_ = 0;
-		}
+		if (!isInGameOver_) {
+			ChackBlock();
+			++growCoolTime_;
+			breakTime_--;
+			if (growCoolTime_ >= growInterval_ && isFlash_ == false) {
+				// 成長
+				SetGrow(nextBlockIndices_, numGrowingBlocks_);
+				// 次成長するところをセット
+				nextBlockIndices_ = GetGrowField(numGrowingBlocks_);
+				growCoolTime_ = 0;
+			}
 
 
-		if (breakTime_ > 0) {
+			if (breakTime_ > 0) {
 
-		}
-		else {
-			if (isFlash_ == true) {
-				isFlash_ = false;
-				for (uint32_t x = 0; x < kNumVerticalBlocks; x++) {
-					for (uint32_t y = 0; y < kNumHorizontalBlocks; y++) {
-						if (blocks_[x][y] == BlockType::Frash) {
-							blocks_[x][y] = BlockType::None;
+			}
+			else {
+				if (isFlash_ == true) {
+					isFlash_ = false;
+					for (uint32_t x = 0; x < kNumVerticalBlocks; x++) {
+						for (uint32_t y = 0; y < kNumHorizontalBlocks; y++) {
+							if (blocks_[x][y] == BlockType::Frash) {
+								blocks_[x][y] = BlockType::None;
+							}
 						}
 					}
+					DownBlockHorizon();
 				}
-				DownBlockHorizon();
 			}
 		}
-	}
-	else {
-		GameOverUpdate();
-	}
+		else {
+			GameOverUpdate();
+		}
 }
 
 void Field::Draw() {
-    TOMATOsEngine::SetBlendMode(kBlendModeNormal);
-    DrawBlock();
-    DrawGrow();
+	TOMATOsEngine::SetBlendMode(kBlendModeNormal);
+	// でっとライン
+	const float kDeadLineAnimationCount = 2.0f;
+	if (deadLineAnimationFront_>= deadLineSize_.x) {
+		deadLineAnimationFront_ = 0.0f;
+		deadLineAnimationBack_ = deadLineSize_.x;
+	}
+	deadLineAnimationFront_ += kDeadLineAnimationCount;
+	deadLineAnimationBack_ += kDeadLineAnimationCount;
+
+	TOMATOsEngine::DrawSpriteRectAngle(deadLinePosition_, deadLineSize_, { 0.5f,0.5f }, 0.0f, { deadLineAnimationFront_ ,16.0f }, deadLineSize_, textureHandles_.at(Texture::kDeadLine), Color::HSVA(deadLineColorH_, 0.8f, 0.8f));
+	DrawBlock();
+	DrawGrow();
 }
 
 void Field::DrawBlock() {
@@ -159,10 +176,10 @@ void Field::DrawBlock() {
 		Vector2 blockPos{};
 		blockPos.x += block->position_.x + rnd.NextFloatRange(-distance, distance);
 		blockPos.y += block->position_.y + rnd.NextFloatRange(-distance, distance);
-		TOMATOsEngine::DrawSpriteRectAngle(blockPos, Vector2(32.0f, 32.0f), Vector2(0.5f, 0.5f), block->angle_, {}, Vector2(32.0f, 32.0f), textureHandles_.at(Texture::kGameOverBlock), Color(0.5f,0.5f,0.5f,0.5f));
+		TOMATOsEngine::DrawSpriteRectAngle(blockPos, Vector2(32.0f, 32.0f), Vector2(0.5f, 0.5f), block->angle_, {}, Vector2(32.0f, 32.0f), textureHandles_.at(Texture::kGameOverBlock), Color(0.5f, 0.5f, 0.5f, 0.5f));
 	}
 	if (isTextDropping_) {
-		TOMATOsEngine::DrawSpriteRectAngle(gameOverPosition_, Vector2(320.0f, 240.0f), Vector2(0.5f, 0.5f),0.0f, {}, Vector2(320.0f, 240.0f), textureHandles_.at(Texture::kGameOver), Color(0.5f,0.5f,0.5f,0.5f));
+		TOMATOsEngine::DrawSpriteRectAngle(gameOverPosition_, Vector2(320.0f, 240.0f), Vector2(0.5f, 0.5f), 0.0f, {}, Vector2(320.0f, 240.0f), textureHandles_.at(Texture::kGameOver), Color(0.5f, 0.5f, 0.5f, 0.5f));
 
 	}
 }
@@ -198,7 +215,7 @@ void Field::GameOverUpdate() {
 			for (uint32_t x = 0; x < kNumHorizontalBlocks; x++) {
 				if (blocks_[x][heightCount_] == BlockType::Normal) {
 					blocks_[x][heightCount_] = BlockType::None;
-					GameOver* gameOver=new GameOver();
+					GameOver* gameOver = new GameOver();
 					// ポジション
 					gameOver->position_ = { float(x * kBlockSize) + float(kBlockSize) * 0.5f,float(heightCount_ * kBlockSize) + float(kBlockSize) * 0.5f };
 					// 速度
@@ -250,7 +267,7 @@ void Field::GameOverUpdate() {
 			t = ((2.0f * t) * (2.0f * t) * ((c2 + 1.0f) * 2.0f * t - c2)) / 2.0f;
 		}
 		else {
-			t = ((2.0f * t - 2.0f)*(2.0f * t - 2.0f)*((c2 + 1.0f) * (t * 2.0f - 2.0f) + c2) + 2.0f) / 2.0f;
+			t = ((2.0f * t - 2.0f) * (2.0f * t - 2.0f) * ((c2 + 1.0f) * (t * 2.0f - 2.0f) + c2) + 2.0f) / 2.0f;
 		}
 		gameOverPosition_.y = Math::Lerp(t, gameOverPositionStart_.y, gameOverPositionEnd_.y);
 
@@ -258,13 +275,13 @@ void Field::GameOverUpdate() {
 			if (!block->isDrop_ && block->position_.y > gameOverPosition_.y - float(TOMATOsEngine::kMonitorHeight) * 0.5f) {
 				block->isDrop_ = true;
 			}
-			else if(block->isDrop_){
+			else if (block->isDrop_) {
 				block->velocity_.y += kGravity;
 				block->acceleration_ += block->velocity_;
 				block->position_ += block->acceleration_;
 				block->acceleration_ = { 0.0f,0.0f };
 				block->angle_ += block->addAngle_;
-				if (!block->isCount_&&block->position_.y < 0.0f - 32.0f) {
+				if (!block->isCount_ && block->position_.y < 0.0f - 32.0f) {
 					block->isCount_ = true;
 					gameOverBlockCount_++;
 				}
@@ -287,32 +304,32 @@ void Field::ColorClearBlock() {
 }
 
 void Field::SetColorBlock(uint32_t blockIndexX, uint32_t blockIndexY, const Vector4& color) {
-    assert(IsInField(blockIndexX, blockIndexY));
-    blocksColor_[blockIndexX][blockIndexY] = color;
+	assert(IsInField(blockIndexX, blockIndexY));
+	blocksColor_[blockIndexX][blockIndexY] = color;
 }
 void Field::BreakBlock(uint32_t blockIndexX, uint32_t blockIndexY) {
-    assert(IsInField(blockIndexX, blockIndexY));
+	assert(IsInField(blockIndexX, blockIndexY));
 	if (blocks_[blockIndexX][blockIndexY] == BlockType::Normal) {
 		blocks_[blockIndexX][blockIndexY] = BlockType::None;
 	}
 
-    // 音
-    size_t playHandle = TOMATOsEngine::PlayAudio(breakSoundHandle_);
-    TOMATOsEngine::SetPitch(playHandle, 1.0f);
+	// 音
+	size_t playHandle = TOMATOsEngine::PlayAudio(breakSoundHandle_);
+	TOMATOsEngine::SetPitch(playHandle, 1.0f);
 }
 
 void Field::BreakBlockHorizon(uint32_t blockIndexX, uint32_t blockIndexY) {
-    assert(IsInField(blockIndexX, blockIndexY));
-    for (size_t x = 0; x < kNumHorizontalBlocks; x++) {
-        breakTime_ = kFrashTime;
+	assert(IsInField(blockIndexX, blockIndexY));
+	for (size_t x = 0; x < kNumHorizontalBlocks; x++) {
+		breakTime_ = kFrashTime;
 		blocks_[static_cast<uint32_t>(x)][blockIndexY] = BlockType::Frash;
-    }
-    downBlockIndex_ = blockIndexY + 1;
-    isFlash_ = true;
+	}
+	downBlockIndex_ = blockIndexY + 1;
+	isFlash_ = true;
 
-    // 音 
-    size_t playHandle = TOMATOsEngine::PlayAudio(lineBreakSoundHandle_);
-    TOMATOsEngine::SetVolume(playHandle, 1.2f);
+	// 音 
+	size_t playHandle = TOMATOsEngine::PlayAudio(lineBreakSoundHandle_);
+	TOMATOsEngine::SetVolume(playHandle, 1.2f);
 }
 
 void Field::ClearBreakBlockHorizon() {
@@ -338,73 +355,73 @@ void Field::ClearBreakBlockHorizon() {
 }
 
 void Field::DownBlockHorizon() {
-    // 一列下げる
-    for (size_t x = 0; x < kNumHorizontalBlocks; x++) {
-        for (size_t y = downBlockIndex_; y < kNumVerticalBlocks; y++) {
-            blocks_[static_cast<uint32_t>(x)][static_cast<uint32_t>(y - 1)] = blocks_[static_cast<uint32_t>(x)][static_cast<uint32_t>(y)];
-        }
-    }
+	// 一列下げる
+	for (size_t x = 0; x < kNumHorizontalBlocks; x++) {
+		for (size_t y = downBlockIndex_; y < kNumVerticalBlocks; y++) {
+			blocks_[static_cast<uint32_t>(x)][static_cast<uint32_t>(y - 1)] = blocks_[static_cast<uint32_t>(x)][static_cast<uint32_t>(y)];
+		}
+	}
 }
 
 uint32_t Field::CalcBlockIndexX(float worldPosX) const {
-    float pX = std::clamp(worldPosX, 0.0f, fieldSize_.x);
-    return uint32_t(pX / kBlockSize);
+	float pX = std::clamp(worldPosX, 0.0f, fieldSize_.x);
+	return uint32_t(pX / kBlockSize);
 }
 
 uint32_t Field::CalcBlockIndexY(float worldPosY) const {
-    float pY = std::clamp(worldPosY, 0.0f, fieldSize_.y);
-    return uint32_t(pY / kBlockSize);
+	float pY = std::clamp(worldPosY, 0.0f, fieldSize_.y);
+	return uint32_t(pY / kBlockSize);
 }
 
 Field::BlockType Field::GetBlock(const Vector2& worldPosition) const {
-    return blocks_[CalcBlockIndexX(worldPosition.x)][CalcBlockIndexY(worldPosition.y)];
+	return blocks_[CalcBlockIndexX(worldPosition.x)][CalcBlockIndexY(worldPosition.y)];
 }
 
 Field::BlockType Field::GetBlock(uint32_t worldPosX, uint32_t worldPosY) const {
-    return blocks_[worldPosX][worldPosY];
+	return blocks_[worldPosX][worldPosY];
 }
 
 float Field::GetBlockLeft(uint32_t blockIndexX) const {
-    return float(blockIndexX * kBlockSize);
+	return float(blockIndexX * kBlockSize);
 }
 float Field::GetBlockTop(uint32_t blockIndexY) const {
-    return float(blockIndexY * kBlockSize + kBlockSize);
+	return float(blockIndexY * kBlockSize + kBlockSize);
 
 }
 float Field::GetBlockRight(uint32_t blockIndexX) const {
-    return float(blockIndexX * kBlockSize + kBlockSize);
+	return float(blockIndexX * kBlockSize + kBlockSize);
 
 }
 float Field::GetBlockBottom(uint32_t blockIndexY) const {
-    return float(blockIndexY * kBlockSize);
+	return float(blockIndexY * kBlockSize);
 }
 
 bool Field::IsInField(const Vector2& worldPosition) const {
-    return IsInField(uint32_t(worldPosition.x / kBlockSize), uint32_t(worldPosition.y / kBlockSize));
+	return IsInField(uint32_t(worldPosition.x / kBlockSize), uint32_t(worldPosition.y / kBlockSize));
 }
 
 bool Field::IsInField(uint32_t blockIndexX, uint32_t blockIndexY) const {
-    if (blockIndexX >= 0 && blockIndexX < kNumHorizontalBlocks &&
-        blockIndexY >= 0 && blockIndexY < kNumVerticalBlocks) {
-        return true;
-    }
-    return false;
+	if (blockIndexX >= 0 && blockIndexX < kNumHorizontalBlocks &&
+		blockIndexY >= 0 && blockIndexY < kNumVerticalBlocks) {
+		return true;
+	}
+	return false;
 }
 
 void Field::Edit() {
-    ImGui::Begin("Field");
-    int coolTime = int(growCoolTime_);
-    ImGui::SliderInt("GrowCoolTime", &coolTime, 0, int(growInterval_));
-    growCoolTime_ = uint32_t(coolTime);
+	ImGui::Begin("Field");
+	int coolTime = int(growCoolTime_);
+	ImGui::SliderInt("GrowCoolTime", &coolTime, 0, int(growInterval_));
+	growCoolTime_ = uint32_t(coolTime);
 
-    int interval = int(growInterval_);
-    ImGui::SliderInt("GrowInterbal", &interval, 0, 500);
-    growInterval_ = uint32_t(interval);
+	int interval = int(growInterval_);
+	ImGui::SliderInt("GrowInterbal", &interval, 0, 500);
+	growInterval_ = uint32_t(interval);
 
-    int numB = int(numGrowingBlocks_);
-    ImGui::SliderInt("NumGrowingBlocks", &numB, 0, int(kNumHorizontalBlocks - 1));
-    numGrowingBlocks_ = uint32_t(numB);
-    ImGui::End();
+	int numB = int(numGrowingBlocks_);
+	ImGui::SliderInt("NumGrowingBlocks", &numB, 0, int(kNumHorizontalBlocks - 1));
+	numGrowingBlocks_ = uint32_t(numB);
+	ImGui::End();
 }
 
 void Field::ChackBlock() {
@@ -419,70 +436,70 @@ void Field::ChackBlock() {
 }
 
 void Field::GrowField(uint32_t numBlocks) {
-    assert(numBlocks > 0 && numBlocks < kNumHorizontalBlocks);
+	assert(numBlocks > 0 && numBlocks < kNumHorizontalBlocks);
 
-    // 0~横幅分の数字配列をシャッフルして
-    // numBlocks以下の配列要素のインデックスを成長させる
-    std::vector<uint32_t> blockIndices(kNumHorizontalBlocks);
-    std::iota(blockIndices.begin(), blockIndices.end(), 0);
+	// 0~横幅分の数字配列をシャッフルして
+	// numBlocks以下の配列要素のインデックスを成長させる
+	std::vector<uint32_t> blockIndices(kNumHorizontalBlocks);
+	std::iota(blockIndices.begin(), blockIndices.end(), 0);
 
-    // シャッフルする
-    {
-        uint32_t i = uint32_t(blockIndices.size() - 1);
-        while (i > 0) {
-            uint32_t j = randomNumberGenerator_.NextUIntRange(0, i);
+	// シャッフルする
+	{
+		uint32_t i = uint32_t(blockIndices.size() - 1);
+		while (i > 0) {
+			uint32_t j = randomNumberGenerator_.NextUIntRange(0, i);
 
-            std::swap(blockIndices[i], blockIndices[j]);
+			std::swap(blockIndices[i], blockIndices[j]);
 
-            i--;
-        }
-    }
+			i--;
+		}
+	}
 
-    for (uint32_t i = 0; i < numBlocks; ++i) {
-        // 成長
-        Grow(blockIndices[i]);
-    }
+	for (uint32_t i = 0; i < numBlocks; ++i) {
+		// 成長
+		Grow(blockIndices[i]);
+	}
 }
 
 std::vector<uint32_t> Field::GetGrowField(uint32_t numBlocks) {
-    assert(numBlocks > 0 && numBlocks < kNumHorizontalBlocks);
+	assert(numBlocks > 0 && numBlocks < kNumHorizontalBlocks);
 
-    // 0~横幅分の数字配列をシャッフルして
-    // numBlocks以下の配列要素のインデックスを成長させる
-    std::vector<uint32_t> blockIndices(kNumHorizontalBlocks);
-    std::iota(blockIndices.begin(), blockIndices.end(), 0);
+	// 0~横幅分の数字配列をシャッフルして
+	// numBlocks以下の配列要素のインデックスを成長させる
+	std::vector<uint32_t> blockIndices(kNumHorizontalBlocks);
+	std::iota(blockIndices.begin(), blockIndices.end(), 0);
 
-    // シャッフルする
-    {
-        uint32_t i = uint32_t(blockIndices.size() - 1);
-        while (i > 0) {
-            uint32_t j = randomNumberGenerator_.NextUIntRange(0, i);
+	// シャッフルする
+	{
+		uint32_t i = uint32_t(blockIndices.size() - 1);
+		while (i > 0) {
+			uint32_t j = randomNumberGenerator_.NextUIntRange(0, i);
 
-            std::swap(blockIndices[i], blockIndices[j]);
+			std::swap(blockIndices[i], blockIndices[j]);
 
-            i--;
-        }
-    }
-    return blockIndices;
+			i--;
+		}
+	}
+	return blockIndices;
 }
 
 void Field::Grow(uint32_t horizontalIndex) {
-    auto block = blocks_[horizontalIndex];
-    for (uint32_t i = kNumVerticalBlocks - 1; i > 0; --i) {
-        uint32_t j = i - 1;
-        if (block[j] != BlockType::None) {
-            block[i] = block[j];
-        }
-    }
-    block[0] = BlockType::Normal;
-    blocksColor_[horizontalIndex][0] = initializeColor_;
+	auto block = blocks_[horizontalIndex];
+	for (uint32_t i = kNumVerticalBlocks - 1; i > 0; --i) {
+		uint32_t j = i - 1;
+		if (block[j] != BlockType::None) {
+			block[i] = block[j];
+		}
+	}
+	block[0] = BlockType::Normal;
+	blocksColor_[horizontalIndex][0] = initializeColor_;
 }
 
 void Field::SetGrow(std::vector<uint32_t> blockIndices, uint32_t numBlocks) {
-    for (uint32_t i = 0; i < numBlocks; ++i) {
-        // 成長
-        Grow(blockIndices[i]);
-    }
+	for (uint32_t i = 0; i < numBlocks; ++i) {
+		// 成長
+		Grow(blockIndices[i]);
+	}
 }
 
 int32_t  Field::GetHeightestIndex() {
